c568b3c5f8e841518d42bb107f184d4057901a31
HuMart <manya01@protonmail.com>
2020-02-17 19:20:48 -0500

    finish

--- src/snackmachineApp/Main.java
+++ src/snackmachineApp/Main.java
@@ -18,8 +18,25 @@ public class Main
 		Snack soda = new Snack("Soda", 24, 2.5, drink.getId());
 		Snack water = new Snack("Water", 20, 2.75, drink.getId());
 
-		// customer 1 buys 3 of snack 4, Print Customer 1 cash on hand. Print quantity of snack 4
+		// Customer 1 (Jane) buys 3 of snack 4 (Soda). Print Customer 1 (Jane) Cash on hand. Print quantity of snack 4 (Soda).
 		c1.setCash(soda.buySnack(c1.getCash(), 4));
+
+		// Customer 1 (Jane) buys 1 of snack 3 (Pretzel). Print Customer 1 (Jane) Cash on hand. Print quantity of snack 3 (Pretzel).
+		c1.setCash(pretzel.buySnack(c1.getCash(), 3));
+
+		// Customer 2 (Bob) buys 2 of snack 4 (Sode). Print Customer 2 (Bob) Cash on Hand. Print quantity of snack 4 (Soda).
+		c2.setCash(soda.buySnack(c2.getCash(), 4));
+
+		// Customer 1 (Jane) finds $10. Print Customer 1 (Jane) Cash on Hand.
+		c1.setCash(10);
+
+		// Customer 1 (Jane) buys 1 of snack 2 (Chocolate Bar). Print Customer 1 (Jane) Cash on Hand. Print quantity of snack 2 (Chocolate Bar).
+		c1.setCash(chocobar.buySnack(c1.getCash(), 2));
+		// Add 12 more items to snack 3 (Pretzel). Print quantity of snack 3 (Pretzel).
+		pretzel.addQuantity(12);
+
+		// Customer 2 (Bob) buys 3 of snack 3 (Pretzel). Print Customer 2 (Bob) Cash on hand. Print quantity of snack 3 (Pretzel).
+		c2.setCash(pretzel.buySnack(c1.getCash(), 3));
 	}
 	public static void main(String[] args)
 	{
